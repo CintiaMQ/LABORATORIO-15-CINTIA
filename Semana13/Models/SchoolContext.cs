b53b1f2305a9f16a6e13c529eb709d6c1fd22baf
cintia <cintia.maucaylle@tecsup.edu.pe>
2024-06-29 16:48:53 -0500

    first commit

---
+++ Semana13/Models/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Semana13.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-90J21RF\\SQLEXPRESS; " +
+                "Initial Catalog=SCHOOL2; Integrated Security=True;trustservercertificate=True");
+        }
+    }
+}
